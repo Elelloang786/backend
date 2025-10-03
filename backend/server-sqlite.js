089216dfda27093ec3c902c37966671ebeff23a0
Elelloang786 <elelloangjeminahntaba@gmail.com>
2025-10-03 13:13:39 -0700

    sdfg

--- backend/server-sqlite.js
+++ backend/server-sqlite.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// SQLite database setup
+
 const dbPath = path.join(__dirname, "database.sqlite")
 const db = new sqlite3.Database(dbPath)
 
